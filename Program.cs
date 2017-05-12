7da8c7151863018b87e15b69ed758d79b8c3a5cc
WIN_NT\x16j050 <x16j050@chiba-fjb.ac.jp>
2017-05-12 11:39:05 +0900

    0.5に変更

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace Example6
             double pai;
             string name;
 
-            score = 25;
+            score = (int)0.5;
             pai = 3.1415;
             name = "TARO";
 
